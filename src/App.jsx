f9205c8f80cabb8e6513e9a3d1eba7e39245e964
Cris <cristinamaillo.r@gmail.com>
2023-07-25 18:35:03 +0100

    started React app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+import Section from './components/Section'
+
+function App() {
+  return (
+    <>
+      <Section />
+      <Section />
+      <Section />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+function Section() {
+    return (
+      <div>
+          <h1>Section</h1>
+          <h1>Section</h1>
+      </div>
+    );
+  }
+  
+  export default Section;
